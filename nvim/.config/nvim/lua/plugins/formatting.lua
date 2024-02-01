7994b09f927f6b951b23fcec467a4f9b264d0684
Luis Cadillo <luiscaf3r@gmail.com>
2024-02-01 12:23:44 -0500

    add lsp_fallback to conform.nvim on-save formatting

---
+++ nvim/.config/nvim/lua/plugins/formatting.lua
@@ -0,0 +1,26 @@
+return {
+	"stevearc/conform.nvim",
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	init = function()
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				require("conform").format({ bufnr = args.buf, lsp_fallback = true })
+			end,
+		})
+	end,
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+		},
+	},
+}
